793a9583947647f9c3331f670f6c8a539cc241a2
Olude Fiyinfoluwa <120565527+Oludefiyinfoluwa06@users.noreply.github.com>
2024-07-15 09:12:13 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
                     <li><NavLink class="nav-link active" to="https://niphal.com.ng/about.html">About</NavLink></li>
                     <li><NavLink class="nav-link scrollto" to="https://niphal.com.ng/index.html#team">Team</NavLink></li>
                     <li><NavLink class="nav-link scrollto" to="https://niphal.com.ng/index.html#contact">Contact</NavLink></li>
-                    <li><NavLink class="nav-link" to="https://niphal.com.ng/klacify.html">Klacify</NavLink></li>
+                    // <li><NavLink class="nav-link" to="https://niphal.com.ng/klacify.html">Klacify</NavLink></li>
                     <li><NavLink class="nav-link" to="https://niphal-blog.vercel.app">Blog</NavLink></li>
                     <li><NavLink class="getstarted" to="https://niphal.com.ng/index.html#contact">Get Started</NavLink></li>
                 </ul>
